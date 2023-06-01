8338a011bf85b5f1bf9550a0715a2fc85d7bdbee
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-06-01 17:57:30 -0400

    added event handler to before install prompt

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const objStore = tx.objectStore('jate');
@@ -22,10 +22,19 @@ export const putDb = async (content) => {
   const res = await req;
   console.log('data saved to the jateDB', res);
 
-}
+};
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+   console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value })
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+
+};
 
 initdb();
