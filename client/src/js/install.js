fd1bf2d9bcc688a3b97354621568544d6251b585
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-06-01 18:17:09 -0400

    implemented click event for install element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,21 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    installBtn.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
